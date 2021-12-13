25c60a2836dd85d2cc7f554d2e98f680b9a3946a
Debjyoti Mohapatra <debjyoti.mohapatra@elever.app>
2021-12-13 21:18:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+let corsOptions = {
+  origin: "http://localhost:8080",
+};
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+const port = 9005;
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
